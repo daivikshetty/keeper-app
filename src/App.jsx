52430457fdd8f17bc0027914f571d6649c3e40a2
Daivik Shetty <shettydaivik100@gmail.com>
2022-08-16 11:08:30 +0530

    changes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,19 @@ import Footer from "./Footer";
 import Note from "./Note";
 import notes from "./notes"
 
-function createNote(notes){
-      return (
-            <Note 
-                  key={notes.id}
-                  title={notes.title}
-                  content={notes.content}
-            />
-      );
-}
 
 
 function App(){
       return (
             <div>
                   <Header />
-                  <div>
-                        {notes.map(createNote)}
-                  </div>
+                        {notes.map((notes)=>{
+                                    <Note 
+                                          key={notes.id}
+                                          title={notes.title}
+                                          content={notes.content}
+                                    />
+                        })}
                   <Footer />
             </div>
       );
