2bae512bd61279655d05f6a586adf1ea1fad1c40
Daivik Shetty <shettydaivik100@gmail.com>
2022-08-16 10:58:25 +0530

    add content file

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,26 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "./notes"
+
+function createNote(notes){
+      return (
+            <Note 
+                  key={notes.id}
+                  title={notes.title}
+                  content={notes.content}
+            />
+      );
+}
+
 
 function App(){
       return (
             <div>
                   <Header />
-                  <Note />
+                  <div>
+                        {notes.map(createNote)}
+                  </div>
                   <Footer />
             </div>
       );
